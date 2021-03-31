41c581cb35efd8cef7419a8095fc335e6cd5e09a
wanglint <wanglint@enn.cn>
2021-03-31 10:00:55 +0800

    添加钱包API接口

--- fch-api/src/main/java/org/freecash/controller/api/WalletApi.java
+++ fch-api/src/main/java/org/freecash/controller/api/WalletApi.java
@@ -1,20 +1,28 @@
 package org.freecash.controller.api;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.wallet.DeterministicSeed;
 import org.bitcoinj.wallet.Wallet;
 import org.bouncycastle.util.encoders.Hex;
+import org.freecash.core.client.FchdClient;
+import org.freecash.core.domain.RawInput;
+import org.freecash.core.domain.RawTransaction;
 import org.freecash.core.param.FchNetWorkParams;
 import org.freecash.core.util.FreeCashUtil;
+import org.freecash.domain.Feip3;
 import org.freecash.dto.*;
+import org.freecash.service.Feip3Service;
 import org.freecash.utils.HttpResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.SecureRandom;
 import java.util.List;
+import java.util.Objects;
 
 @CrossOrigin(maxAge = 3600)
 @Api(tags = "钱包API接口")
@@ -22,6 +30,9 @@ import java.util.List;
 @RequestMapping("api/v1/tool/wallet")
 @RequiredArgsConstructor
 public class WalletApi {
+
+    private final Feip3Service feip3Service;
+    private final FchdClient fchdClient;
 
     @ApiOperation(value = "获取助记符，公钥，私钥和地址")
     @GetMapping("info")
@@ -46,5 +57,24 @@ public class WalletApi {
         return HttpResult.SUCCESS(ValidMessageResponse.builder()
                 .code(0).message("").valid(res)
                 .build());
+    }
+
+    @ApiOperation(value = "通过地址获取公钥")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "address", value = "地址", required = true, dataType = "string")
+    })
+    @PostMapping("address/pk")
+    public HttpResult<String> getPublicKey(String address) throws Exception{
+        Feip3 feip3 = feip3Service.getFeip3ByAddress(address);
+        if(Objects.isNull(feip3)){
+            return HttpResult.FAIL(100,"该地址暂未注册CID","");
+        }
+        String txId = feip3.getTxHash();
+        RawTransaction rawTransaction = (RawTransaction)fchdClient.getRawTransaction(txId, true);
+        RawInput input = rawTransaction.getVIn().get(0);
+        String asm = input.getScriptSig().getAsm();
+        String[] res = asm.split("\\[ALL\\|FORKID\\]");
+        String pk = res.length == 1 ? "" : res[1].trim();
+        return HttpResult.SUCCESS(pk);
     }
 }
